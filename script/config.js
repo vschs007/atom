86eee72a2e3e8ae816f2e07e182939082ede768d
Vivek Kumar Singh <vschs007@gmail.com>
2017-04-04 12:10:26 +0530

    updated
    space efficient

--- script/config.js
+++ script/config.js
@@ -1,11 +1,8 @@
 // This module exports paths, names, and other metadata that is referenced
 // throughout the build.
-
 'use strict'
-
 const fs = require('fs')
 const path = require('path')
-
 const repositoryRootPath = path.resolve(__dirname, '..')
 const apmRootPath = path.join(repositoryRootPath, 'apm')
 const scriptRootPath = path.join(repositoryRootPath, 'script')
@@ -16,11 +13,9 @@ const symbolsPath = path.join(buildOutputPath, 'symbols')
 const electronDownloadPath = path.join(repositoryRootPath, 'electron')
 const homeDirPath = process.env.HOME || process.env.USERPROFILE
 const atomHomeDirPath = process.env.ATOM_HOME || path.join(homeDirPath, '.atom')
-
 const appMetadata = require(path.join(repositoryRootPath, 'package.json'))
 const apmMetadata = require(path.join(apmRootPath, 'package.json'))
 const channel = getChannel()
-
 module.exports = {
   appMetadata, apmMetadata, channel,
   repositoryRootPath, apmRootPath, scriptRootPath,
@@ -29,7 +24,6 @@ module.exports = {
   getApmBinPath, getNpmBinPath,
   snapshotAuxiliaryData: {}
 }
-
 function getChannel () {
   if (appMetadata.version.match(/dev/)) {
     return 'dev'
@@ -39,12 +33,10 @@ function getChannel () {
     return 'stable'
   }
 }
-
 function getApmBinPath () {
   const apmBinName = process.platform === 'win32' ? 'apm.cmd' : 'apm'
   return path.join(apmRootPath, 'node_modules', 'atom-package-manager', 'bin', apmBinName)
 }
-
 function getNpmBinPath () {
   const npmBinName = process.platform === 'win32' ? 'npm.cmd' : 'npm'
   const localNpmBinPath = path.resolve(repositoryRootPath, 'script', 'node_modules', '.bin', npmBinName)
